5e1fa23ec82ad1cfffb0ca417c945e1a554a04c4
Slin castro <scastro@thoughtworks.com>
2017-07-17 18:32:37 -0400

    adding turn right and left

--- control/motor.py
+++ control/motor.py
@@ -2,16 +2,21 @@
 
 class Motor():
     def __init__(self):
-        self._left = False
-        self._right = False
+        self._inputA = False
+        self._inputB = False
 
     @property
+    def InputA(self):
+        return self._inputA
+
+    @property
+    def InputB(self):
+        return self._inputB
+
     def Left(self):
-        return self._left
+        self._inputA=True
+        self._inputB=False
 
-    @property
     def Right(self):
-        return self._right
-
-    def on(self):
-        self._left=True
+        self._inputB=True
+        self._inputA=False
--- control/test_motor.py
+++ control/test_motor.py
@@ -4,11 +4,29 @@ from motor import Motor
 
 
 class TestControl(unittest.TestCase):
-    def test_should_return_on_engine(self):
+    def test_should_turn_left(self):
         shoulder=Motor()
-        shoulder.on()
-        self.assertEqual(True,shoulder.Left)
-        self.assertEqual(False,shoulder.Right)
+        shoulder.Left()
+        self.assertEqual(True,shoulder.InputA)
+        self.assertEqual(False,shoulder.InputB)
+
+    def test_should_turn_right(self):
+        shoulder=Motor()
+        shoulder.Right()
+        self.assertEqual(True,shoulder.InputB)
+        self.assertEqual(False,shoulder.InputA)
+
+    def test_should_turn_Left_and_then_turn_right(self):
+        shoulder=Motor()
+
+        shoulder.Right()
+        self.assertEqual(True,shoulder.InputB)
+        self.assertEqual(False,shoulder.InputA)
+
+        shoulder.Left()
+        self.assertEqual(True,shoulder.InputA)
+        self.assertEqual(False,shoulder.InputB)
+
 
 if __name__ == '__main__':
   unittest.main()
