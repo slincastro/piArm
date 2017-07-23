3b85a04b6ad0137f96c4ae2ba6a223e11461c6af
Slin castro <scastro@thoughtworks.com>
2017-07-23 00:59:00 -0400

    adding shoulder configuration

--- src/main/configuration/configuration.py
+++ src/main/configuration/configuration.py
@@ -3,7 +3,7 @@ import ConfigParser
 
 class Configuration():
 
-    def configure_test(self):
+    def configure(self):
         config = ConfigParser.RawConfigParser()
         config.add_section('Constants')
         config.set('Constants', 'coefficient_conversion', '100.00')
--- src/main/pi_arm.py
+++ src/main/pi_arm.py
@@ -6,15 +6,16 @@ from src.main.motor import Motor
 from src.main.configuration.configuration import Configuration
 from src.main.arm import Arm
 
-Configuration().configure_test()
+Configuration().configure()
 executor = Executor()
 waist = Motor(13, 19)
-arm = Arm(waist)
+shoulder = Motor(23, 24)
+arm = Arm(waist, shoulder)
 
 while True:
     junture = raw_input("enter an junture <w>: ")
     angle = raw_input("enter an angle <int>: ")
-    direction =raw_input("enter a direction <l|r>")
+    direction = raw_input("enter a direction <l|r>")
 
     motor = arm.get_motor(junture)
     control = Control(motor, executor)
