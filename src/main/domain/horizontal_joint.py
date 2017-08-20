5c367f877f29b250280aefb172509f447d59b098
Slin castro <scastro@thoughtworks.com>
2017-08-19 20:08:05 -0500

    adding right move

--- src/main/domain/horizontal_joint.py
+++ src/main/domain/horizontal_joint.py
@@ -2,6 +2,14 @@ from src.main.domain.joint import Joint
 
 
 class HorizontalJoint(Joint):
-
     def left(self):
-        self.motor.turn_left()
+        if self.encoder >= 150:
+            self.motor.stop()
+        else:
+            self.motor.turn_left()
+
+    def right(self):
+        if self.encoder >= 150:
+            self.motor.stop()
+        else:
+            self.motor.turn_right()
--- src/test/test_horizontal_joint.py
+++ src/test/test_horizontal_joint.py
@@ -18,5 +18,66 @@ class TestHorizontalJoint(unittest.TestCase):
 
         self.assertTrue(joint.motor.is_on())
 
+    def test_should_stop_when_encoder_value_equals_150(self):
+        current_joint_id = "w"
+        current_joint_name = "waist"
+        current_motor = Motor(1, 2)
+        current_encoder_value = 150
+
+        joint = HorizontalJoint(current_joint_id, current_joint_name, current_motor, current_encoder_value)
+
+        joint.left()
+
+        self.assertFalse(joint.motor.is_on())
+
+    def test_should_stop_when_encoder_value_more_than_150(self):
+        current_joint_id = "w"
+        current_joint_name = "waist"
+        current_motor = Motor(1, 2)
+        current_encoder_value = 151
+
+        joint = HorizontalJoint(current_joint_id, current_joint_name, current_motor, current_encoder_value)
+
+        joint.left()
+
+        self.assertFalse(joint.motor.is_on())
+
+    def test_should_move_right_when_encoder_Value_less_than_150(self):
+        current_joint_id = "w"
+        current_joint_name = "waist"
+        current_motor = Motor(1, 2)
+        current_encoder_value = 60
+
+        joint = HorizontalJoint(current_joint_id, current_joint_name, current_motor, current_encoder_value)
+
+        joint.right()
+
+        self.assertTrue(joint.motor.is_on())
+
+    def test_should_rigth_and_stop_when_encoder_value_equals_150(self):
+        current_joint_id = "w"
+        current_joint_name = "waist"
+        current_motor = Motor(1, 2)
+        current_encoder_value = 150
+
+        joint = HorizontalJoint(current_joint_id, current_joint_name, current_motor, current_encoder_value)
+
+        joint.right()
+
+        self.assertFalse(joint.motor.is_on())
+
+    def test_should_right_and_stop_when_encoder_value_more_than_150(self):
+        current_joint_id = "w"
+        current_joint_name = "waist"
+        current_motor = Motor(1, 2)
+        current_encoder_value = 151
+
+        joint = HorizontalJoint(current_joint_id, current_joint_name, current_motor, current_encoder_value)
+
+        joint.right()
+
+        self.assertFalse(joint.motor.is_on())
+
+
 if __name__ == '__main__':
     unittest.main()
